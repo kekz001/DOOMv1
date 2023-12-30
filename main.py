a16cc058aac986a514e25ff382ce81b0cb5891b6
Klleriston <Klleandrade@outlook.com>
2023-12-29 21:31:38 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import pygame as pg
+import sys
+from settings import *
+
+class Game:
+    def __init__(self):
+       #Criar a tela para renderizar a resolução do jogo 
+        pg.init()
+        self.screen = pg.display.set_mode(RES)
+        self.clock = pg.time.Clock()
+
+    def new_game(self):
+        pass
+
+    #Atualização da tela para mostrar os FPS
+    def update(self):
+        pg.display.flip()
+        self.clock.tick(FPS)
+        pg.display.set_caption(f'{self.clock.get_fps() : .1f}')
+    
+    #A cada interação pintar a tela de preto
+    def draw(self):
+        self.screen.fill('black')
+    
+    #Checar eventos por ex se apertamos a tecla ESC para sair do game
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+    
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
+ 
---
+++ settings.py
@@ -0,0 +1,3 @@
+#Configuração do jogo
+RES = WIDTH, HEIGHT = 800, 600;
+FPS = 60;
